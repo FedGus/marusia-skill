9744c10f756ed6de4b73b3b8788502540bcb70cc
Fedor Gusev <gusevfedya@mail.ru>
2021-04-07 11:54:53 +0300

    creating conditions for guessing a word

--- create-response.js
+++ create-response.js
@@ -1,11 +1,26 @@
-const { pick } = require('ramda')
+const { pick } = require("ramda");
 
-module.exports = ({ request, session, version }) => ({
-  response: {
-    text: "Угадали! Эта буква встречается " + 2 + " раза",
-    tts: "Угадали! Эта буква встречается " + 2 + " раза",
-    end_session: false,
+module.exports = {
+  youGuessed: function ({ request, session, version }) {
+    return {
+      response: {
+        text: "Угадали! Эта буква встречается " + 2 + " раза",
+        tts: "Угадали! Эта буква встречается " + 2 + " раза",
+        end_session: false
+      },
+      session: pick(["session_id", "message_id", "user_id"], session),
+      version
+    };
   },
-  session: pick(['session_id', 'message_id', 'user_id'], session),
-  version,
-})
+  youDidntGuess: function ({ request, session, version }) {
+    return {
+      response: {
+        text: "Не угадали! У вас осталась " + 1 + " попытка",
+        tts: "Не угадали! У вас осталась " + 1 + " попытка",
+        end_session: false
+      },
+      session: pick(["session_id", "message_id", "user_id"], session),
+      version
+    };
+  }
+};
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const server = express();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
-const createEchoResponse = require('./create-response.js')
-const { pick } = require('ramda')
+const createEchoResponse = require("./create-response.js");
+const { pick } = require("ramda");
+let word = "сообщение";
 
 // Настройка CORS
 server.use(function (req, res, next) {
@@ -26,41 +27,47 @@ server.use(bodyParser.json());
 // Парсинг запросов по типу: application/x-www-form-urlencoded
 server.use(
   bodyParser.urlencoded({
-    extended: true,
+    extended: true
   })
 );
 
-server
-  .post("/webhook", (req, res) => {
-    try {
-      if (req.body.session['new'] == true) {
-        res.json({
-          response: {
-            text: "Привет! Давайте сыграем в виселицу. Попробуйте отгадать мое слово по буквам! Но помните - у вас всего девять попыток",
-            tts: "Привет! Давайте сыграем в виселицу. Попробуйте отгадать мое слово по буквам! Но помните - у вас всего девять попыток",
-            buttons: [
-              {
-                title: "Старт",
-                payload: {},
+server.post("/webhook", (req, res) => {
+  try {
+    if (req.body.session["new"] == true) {
+      res.json({
+        response: {
+          text:
+            "Привет! Давайте сыграем в виселицу. Попробуйте отгадать мое слово по буквам! Но помните - у вас всего девять попыток",
+          tts:
+            "Привет! Давайте сыграем в виселицу. Попробуйте отгадать мое слово по буквам! Но помните - у вас всего девять попыток",
+          buttons: [
+            {
+              title: "Старт",
+              payload: {},
               url: ""
-              }
-            ],
-            end_session: false,
-          },
-          session: pick(['session_id', 'message_id', 'user_id'], req.body.session),
-          version: req.body.version,
-        })
-      } else {
-        res.json(createEchoResponse(req.body))
+            }
+          ],
+          end_session: false
+        },
+        session: pick(
+          ["session_id", "message_id", "user_id"],
+          req.body.session
+        ),
+        version: req.body.version
+      });
+    } else {
+      if (word.indexOf(req.body.request["command"]) > -1)
+        res.json(createEchoResponse.youGuessed(req.body));
+      else {
+        res.json(createEchoResponse.youDidntGuess(req.body));
       }
-    } catch (err) {
-      console.log("Ошибка ", err);
-      res.status(500).send(err);
     }
+  } catch (err) {
+    console.log("Ошибка ", err);
+    res.status(500).send(err);
+  }
+});
 
-  })
-
-server
-  .listen(port, () => {
-    console.log("Сервер запущен на http://localhost:" + port);
-  });
+server.listen(port, () => {
+  console.log("Сервер запущен на http://localhost:" + port);
+});
