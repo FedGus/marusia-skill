316c61353dd25fe9f1d33a200ff8ac054a2fa940
Fedor <gusevfedya@mail.ru>
2021-04-10 18:05:14 +0300

    displaying the pattern of the hidden word

--- create-response.js
+++ create-response.js
@@ -1,7 +1,10 @@
 const { pick } = require("ramda");
 
 let word = "";
+let hideWord = "";
+let arrWord = [];
 let attempts = 2;
+let count;
 var arr = ['сообщение', 'колодец', 'праздник', 'картинка', 'троллейбус', 'каравай', 'поступок'];
 
 function arrayRandElement(arr) {
@@ -17,12 +20,24 @@ function pluralizeRus(n, forms) {
         || n % 100 >= 20) ? forms[1] : forms[2]);
 }
 
-function pattern(word) {
-  let pattern = "";
-  for (let i = 1; i < word.length; i++) {
-    pattern += " _ ";
+function newHideWord(word) {
+  for (let i = 0; i < word.length; i++) {
+    arrWord[i] = word.charAt(i)
+    console.log(arrWord[i]);
+    hideWord += "*";
   }
-  return pattern;
+  return hideWord;
+}
+
+function pattern(letter) {
+  count = 0;
+  for (let i = 0; i < arrWord.length; i++) {
+    if (letter == arrWord[i]) {
+    hideWord = hideWord.substr(0, i) + letter + hideWord.substr(i + 1);
+    count++;
+    }
+  }
+  return {hideWord, count};
 }
 
 
@@ -31,9 +46,13 @@ function pattern(word) {
 module.exports = {
   loose: function ({ request, session, version }) {
     if (attempts <= 0) {
+      hideWord = "";
+      attempts = 2;
       return this.youResult({ request, session, version })
     } else {
       if (word.indexOf(request["command"]) > -1) {
+        if (hideWord == word)
+        return this.youWin({ request, session, version }); else 
         return this.youGuessed({ request, session, version })
       } else {
         attempts--;
@@ -71,7 +90,7 @@ module.exports = {
     return {
       response: {
         text:
-          ["Я загадала слово из "+ word.length + " букв! Буква?", pattern(word)],
+          ["Я загадала слово из "+ word.length + " букв! Буква?", newHideWord(word)],
         tts:
         "Я загадала слово из "+ word.length + " букв! ^Буква^?",
         end_session: false,
@@ -83,7 +102,7 @@ module.exports = {
   youGuessed: function ({ request, session, version }) {
     return {
       response: {
-        text: "Угадали! Эта буква встречается " + 2 + " раза." + word,
+        text: ["Угадали! Эта буква встречается " + pattern(request.command).count + " раза." , pattern(request.command).hideWord],
         tts: "<speaker audio=\"marusia-sounds/game-ping-1\"> Угадали! Эта буква встречается " + 2 + " раза.",
         end_session: false
       },
@@ -109,6 +128,32 @@ module.exports = {
           "Вы проиграли. Не расстраивайтесь, в следующий раз точно повезет! Поиграем еще?",
         tts:
           "<s>Вы проиграли.</s> Не расстраивайтесь, в следующий раз точно повезет! <break time='1000ms'/> Поиграем ещ`ё?",
+          buttons: [
+        {
+          title: "Да!",
+          payload: {},
+          url: "",
+        },
+        {
+          title: "Нет",
+          payload: { command: "on_interrupt" },
+          url: ""
+        }
+      ],
+        end_session: false
+      },
+      
+      session: pick(["session_id", "message_id", "user_id"], session),
+      version
+    };
+  },
+  youWin: function ({ request, session, version }) {
+    return {
+      response: {
+        text:
+          "Поздравляю, вы выиграли! Да, это слово '"+ word + "'. Поиграем еще?",
+        tts:
+          "<s>Поздравляю, вы выиграли!</s> Да, это слово '"+ word + "'. <break time='1000ms'/> Поиграем ещ`ё?",
           buttons: [
         {
           title: "Да!",
